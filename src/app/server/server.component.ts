c7867af1f8ddf3648d5b5b8fe843a165b47670ac
EGDK\raest <raest@eg.dk>
2023-02-24 21:04:39 +0100

    19 Component templateUrl

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-server',
-    template: 'server.component.html',
-    
+    templateUrl: './server.component.html',
+
 })
 export class ServerComponent {
 
