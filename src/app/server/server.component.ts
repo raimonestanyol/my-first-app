ba95daf53168a1961193c925e6f24edca57f98b8
EGDK\raest <raest@eg.dk>
2023-02-27 13:23:56 +0100

    26 string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,11 @@ import { Component } from "@angular/core";
 
 })
 export class ServerComponent {
+    serverId=10;
+    serverStatus:string = 'offline';
 
+    //just to demonstrate you can call a method from the string interpolation box, you can ofc use the property directly
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
